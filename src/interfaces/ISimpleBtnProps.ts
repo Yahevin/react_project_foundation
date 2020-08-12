1c686bd0bc03f7d9006c1632b2a1f041ade6cfa4
Yahevin <yahevin15@gmail.com>
2020-08-12 12:59:35 +0300

    add onClick handler to SimpleBtn component

--- src/interfaces/ISimpleBtnProps.ts
+++ src/interfaces/ISimpleBtnProps.ts
@@ -1,8 +1,9 @@
 import COLORS from "@/constants/colors";
 
-export default interface SimpleBtnProps {
+export default interface ISimpleBtnProps {
     theme: COLORS,
     children: string,
     disabled: boolean,
     is_large: boolean,
+    callback: (...args:any | undefined) => any | void,
 }
