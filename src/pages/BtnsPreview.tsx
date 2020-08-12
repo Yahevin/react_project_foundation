e135f3a0d3623dbd10735308df57fe07f380e1db
Yahevin <yahevin15@gmail.com>
2020-08-12 13:02:43 +0300

    add xmlHttpRequest

--- src/pages/BtnsPreview.tsx
+++ src/pages/BtnsPreview.tsx
@@ -12,19 +12,54 @@ const StyledList = styled.div`
 `;
 
 function App() {
+    const [message, setMessage] = React.useState('');
+
+    const getResponse = () => {
+        return new Promise<string>((resolve,reject) =>{
+            const xhr = new XMLHttpRequest();
+
+            xhr.open('GET', 'https://httpbin.org/get');
+            xhr.send();
+
+            xhr.onload = () => {
+                return resolve(xhr.responseText);
+            };
+            xhr.onerror = () => {
+                return reject('error');
+            }
+        })
+    };
+
+    const handleClick = async () => {
+        try {
+            const response:string = await getResponse();
+
+            setMessage(response);
+        } catch (error) {
+            setMessage(error);
+        }
+    };
+
     return (
-        <StyledList>
-            {btnList.map((item)=>
-                <SimpleBtn
-                    key={item.id}
-                    theme={item.theme}
-                    disabled={item.disabled}
-                    is_large={item.is_large}
-                >
-                    { item.children }
-                </SimpleBtn>
-            )}
-        </StyledList>
+        <>
+            <StyledList>
+                {btnList.map((item)=>
+                    <SimpleBtn
+                        key={item.id}
+                        theme={item.theme}
+                        disabled={item.disabled}
+                        is_large={item.is_large}
+                        callback={handleClick}
+                    >
+                        { item.children }
+                    </SimpleBtn>
+                )}
+            </StyledList>
+
+            <p>
+                { message }
+            </p>
+        </>
     );
 }
 
