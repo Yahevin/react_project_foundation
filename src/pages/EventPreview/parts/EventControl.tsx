3b7d131879f3e0a54d6d014cd392ca2ae7f780d7
Yahevin <yahevin15@gmail.com>
2020-08-18 10:12:13 +0300

    fix styles of EventPreview component

--- src/pages/EventPreview/parts/EventControl.tsx
+++ src/pages/EventPreview/parts/EventControl.tsx
@@ -1,23 +1,49 @@
 import React from "react";
+import styled from "styled-components";
+
+import CheckBox from "@/components/input/CheckBox";
+
 import IRule from "@/pages/EventPreview/interface/IRule";
 import ITurnHandler from "@/pages/EventPreview/interface/ITurnHandler";
+
+
+const ControlRow = styled.div`
+    display: flex;
+    
+    & + & {
+      margin: 16px 0 0 0;
+    }
+`;
+const ControlRow__item = styled.div`
+    & + & {
+      margin: 0 0 0 16px;
+    }
+`;
+
+
 
 function EventControl(props: {rule:Array<IRule>, callback:ITurnHandler}) {
     return (
         <>
             {props.rule.map((item,index)=>{
                 return (
-                    <div>
-                        <input type="checkbox"
-                               id={"stop-btn-" + index}
-                               onChange={()=>{props.callback('stop', !item.stop, index)}}/>
-                        <label htmlFor={"stop-btn-" + index}>Stop {index}</label>
+                    <ControlRow key={item.id}>
+                        <ControlRow__item as={CheckBox}
+                            id={"stop-btn-" + item.id}
+                            checked={item.stop}
+                            callback={()=>{props.callback('stop', !item.stop, item.id)}}
+                        >
+                            Stop {item.id}
+                        </ControlRow__item>
 
-                        <input type="checkbox"
-                               id={"capture-btn-" + index}
-                               onChange={()=>{props.callback('capture',!item.capture, index)}}/>
-                        <label htmlFor={"capture-btn-" + index}>Capture {index}</label>
-                    </div>
+                        <ControlRow__item as={CheckBox}
+                            id={"capture-btn-" + item.id}
+                            checked={item.capture}
+                            callback={()=>{props.callback('capture',!item.capture, item.id)}}
+                        >
+                            Capture {item.id}
+                        </ControlRow__item>
+                    </ControlRow>
                 )
             })}
         </>
