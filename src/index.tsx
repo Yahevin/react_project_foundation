4d4faef4840e2a7e75bd16ddcc5ff87ad07d030c
Yahevin <yahevin15@gmail.com>
2020-08-18 10:13:51 +0300

    rename styles directory

--- src/index.tsx
+++ src/index.tsx
@@ -2,8 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
 import IndexPage from "@/pages/IndexPage";
-import "@/styles/reset.css";
-import "@/styles/base.css";
+import "@/global-styles/reset.css";
+import "@/global-styles/base.css";
 
 ReactDOM.render(
     <Router>
