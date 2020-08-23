9bcb38a525744b027f7136fceabe61633e5e0d77
Yahevin <yahevin15@gmail.com>
2020-08-23 20:46:20 +0300

    update colors enum

--- src/styled-components/Header.js
+++ src/styled-components/Header.js
@@ -1,10 +1,11 @@
 import styled from "styled-components";
+import COLORS from "@/constants/colors";
 
 const Header = styled.header`
     width: 100%;
     height: 60px;
-    border-bottom: 2px solid #4387cc;
-    background: #454545;
+    border-bottom: 2px solid ${COLORS.light_grey};
+    background: ${COLORS.grey};
 `;
 
 export default Header;
