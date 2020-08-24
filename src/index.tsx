c33a6c21f50d726d598e76f0294b88e2c9daf3a2
Yahevin <yahevin15@gmail.com>
2020-08-24 13:38:29 +0300

    set the dinamic basename to react-router
    it is needed for production build

--- src/index.tsx
+++ src/index.tsx
@@ -5,8 +5,12 @@ import IndexPage from "@/pages/IndexPage";
 import "@/global-styles/reset.css";
 import "@/global-styles/base.css";
 
+const basePath = window.location.pathname.indexOf('react_project_foundation') !== -1
+    ? '/react_project_foundation/dist'
+    : '';
+
 ReactDOM.render(
-    <Router>
+    <Router basename={basePath}>
         <IndexPage/>
     </Router>,
     document.getElementById('app')
