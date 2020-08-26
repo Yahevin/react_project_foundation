056609891b10b9c1cd7a99104723638c38a8fa88
Yahevin <yahevin15@gmail.com>
2020-08-26 10:34:22 +0300

    create demo with useMemo tricks

---
+++ src/pages/MemoHook/index.tsx
@@ -0,0 +1,76 @@
+import React, {useMemo, useState} from "react";
+import {Presentation, Presentation__control, Presentation__view} from "@styled/Presentation";
+import UpdateAttention from "@/pages/MemoHook/parts/UpdateAttention";
+import SimpleBtn from "@/components/btn/SimpleBtn";
+import CenteredSb from "@styled/flex/CenteredSb";
+import COLORS from "@/constants/colors";
+import styled from "styled-components";
+
+const VerticalList = styled(Presentation__control)`
+  display: grid;
+  grid-gap: 12px 0;
+  grid-template-columns: 1fr;
+`;
+
+function MemoHook() {
+    const [necessary,setNecessary] = useState(0);
+    const [useful,setUseful] = useState(0);
+
+    const MemoizedChild = useMemo(()=>
+            <UpdateAttention necessary={necessary}>
+                Memoized
+            </UpdateAttention>
+    ,[necessary]);
+
+    const memoizedProp = useMemo(()=>({prop: necessary}),[necessary]);
+
+    return (
+        <Presentation>
+            <Presentation__view>
+                <VerticalList>
+                    <h3>
+                        See console
+                    </h3>
+                    <div>
+                        necessary: {necessary}
+                    </div>
+                    <div>
+                        useful: {useful}
+                    </div>
+                    <CenteredSb>
+                        <UpdateAttention necessary={necessary}>
+                            Default
+                        </UpdateAttention>
+
+                        <UpdateAttention necessary={memoizedProp.prop}>
+                            With memoized prop
+                        </UpdateAttention>
+
+                        {MemoizedChild}
+                    </CenteredSb>
+                </VerticalList>
+            </Presentation__view>
+            <Presentation__control>
+                <VerticalList>
+                    <SimpleBtn
+                        theme={COLORS.orange}
+                        disabled={false}
+                        is_large={true}
+                        callback={()=>{setNecessary(necessary + 1)}}>
+                        + update children props
+                    </SimpleBtn>
+
+                    <SimpleBtn
+                        theme={COLORS.orange}
+                        disabled={false}
+                        is_large={true}
+                        callback={()=>{setUseful(useful + 1)}}>
+                        + update useful state
+                    </SimpleBtn>
+                </VerticalList>
+            </Presentation__control>
+        </Presentation>
+    )
+}
+
+export default MemoHook;
